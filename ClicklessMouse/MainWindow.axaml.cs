eecde7dbf9858190242158d84e14943f36538af0
Garrett Anderson <gband85@mailfence.com>
2025-05-08 20:31:40 -0500

    create new AvaloniaUI project

---
+++ ClicklessMouse/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ClicklessMouse
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
