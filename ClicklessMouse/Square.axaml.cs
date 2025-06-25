f57e9c13fb0a3c5a57eee520ef12f38caf027419
Garrett Anderson <gband85@mailfence.com>
2025-06-25 18:47:41 -0500

    add arguments to square constructor

--- ClicklessMouse/Square.axaml.cs
+++ ClicklessMouse/Square.axaml.cs
@@ -6,7 +6,7 @@ namespace ClicklessMouse;
 
 public partial class Square : Window
 {
-    public Square()
+    public Square(int Side, int Line_width, System.Drawing.Color color1, System.Drawing.Color color2)
     {
         InitializeComponent();
     }
