03c772c49b24c3ddba86565c9b251b519a7c2e2e
Garrett Anderson <gband85@mailfence.com>
2025-06-25 17:09:23 -0500

    add Avalonia square window

---
+++ ClicklessMouse/Square.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ClicklessMouse;
+
+public partial class Square : Window
+{
+    public Square()
+    {
+        InitializeComponent();
+    }
+}
