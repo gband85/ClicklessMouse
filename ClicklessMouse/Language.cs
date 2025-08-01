4aea46a83acbdaaf07aa2b4fcb436319ea45539a
Garrett Anderson <gband85@mailfence.com>
2025-08-01 14:27:27 -0500

    add content back to Language.cs

--- ClicklessMouse/Language.cs
+++ ClicklessMouse/Language.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Windows;
-using System.Windows;
-
-
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Controls.Documents;
+using Avalonia.Media.Imaging;
 using Avalonia.Platform;
-//Documents;
+using AvRichTextBox;
+using DocumentFormat.OpenXml.Bibliography;
+using HarfBuzzSharp;
+using System;
+using System.Windows;
+//using System.Windows.Documents;
 //using System.Windows.Forms;
 //using System.Windows.Media.Imaging;
-using Avalonia.Controls;
-using Avalonia.Media.Imaging;
-using Avalonia.Controls.Documents;
-using Avalonia.Platform;
-//using Screen = System.Windows.Forms.Screen;
-using Window = Avalonia.Controls.Window;
-using System.Security.Policy;
 
 namespace ClicklessMouse
 {
@@ -38,163 +32,142 @@ namespace ClicklessMouse
 
             if (lang == language.en)
             {
+                MIfile.Header = "File";
+                MIexit.Header = "Exit";
+                MIrestore.Header = "Restore";
+                MIdefault_colors.Header = "Default Square Colors";
+                MIdefault_settings.Header = "All Default Settings";
+                MIlanguage.Header = "Language";
+                MIenglish.Header = "English";
+                MIpolish.Header = "Polish";
+                MIhelp.Header = "Help";
+                MImanual.Header = "User Guide";
+                MIabout.Header = "About";
+                GBmain_settings.Header = "Main Settings";
+                CHBLMB.Content = "LMB click";
+                CHBRMB.Content = "RMB click";
+                CHBdoubleLMB.Content = "LMB double click";
+                CHBholdLMB.Content = "LMB holding";
+                CHBholdRMB.Content = "RMB holding";
+                CHBscreen_panning.Content = "Screen panning";
+                Lcursor_idle_before_squares_appear.Text = "Cursor idle time before squares appear [ms]:";
+                Ltime_to_start_mouse.Text = "Time to start mouse movement after squares appear [ms]:";
+                Lcursor_time_in_square.Text = "Cursor time in square to register click [ms]:";
+                GBother_settings.Header = "Other Settings";
+                CHBrun_at_startup.Content = "Run when computer starts";
+                CHBstart_minimized.Content = "Start minimized";
+                CHBminimize_to_tray.Content = "Minimize to system tray";
+                CHBcheck_for_updates.Content = "Check for updates automatically";
+                GBsquare_settings.Header = "Squares Settings";
+                Lsquare_size.Content = "Size [px]:";
+                Lsquare_border.Content = "Border width [px]:";
+                Lsquare_color1.Content = "Color 1:";
+                Lsquare_color2.Content = "Color 2:";
+                Lmin_square_size.Content = "Minimum size [%]:";
+                GBrecommended_square_size.Header = "Recommended Square Size";
+                Lscreen_size.Content = "Diagonal screen size [in]:";
+                contentLscreen_resolution = "Screen resolution: ";
                 //int x = Screen.PrimaryScreen.Bounds.Width;
                 //int y = Screen.PrimaryScreen.Bounds.Height;
-                //TBLscreen_resolution.Text = x + "x" + y;
+                Lscreen_resolution.Content = "Screen resolution: "; 
+                TBscreen_resolution.Text = x + "x" + y;
+                //contentLscreen_resolution + x + "x" + y;
+                Bset_recommended_square.Content = "Set recommended square size";
 
+                error_title = "Error";
+                about_title = "About";
                 about_content = "Version: " + prog_version + "\nAuthor: Mikołaj Magowski";
-
+                argb_error = "ARGB color value must be lower than 0";
+                screen_size_error1 = "Diagonal screen size must be higher than 0";
+                screen_size_error2 = "Diagonal screen size can't be empty";
+                cursor_idle_time_error = "Cursor idle time before squares appear cannot be lower than ";
+                time_to_start_mouse_movement_error = "Time to start mouse movement cannot be lower than ";
+                cursor_time_in_square_error = "Cursor time in square to register click cannot be "
+                    + "lower than ";
+                square_size_error = "Square size cannot be lower than ";
+                square_border_error = "Square border width cannot be lower than ";
+                min_square_size_too_low_error = "Minimum square size cannot be lower than ";
+                min_square_size_too_high_error = "Minimum square size cannot be higher than ";
+                loading_error_msg = " Settings loading error - values that weren't loaded will be " +
+                    "restored to default and saved.";
             }
-            else if(lang == language.pl)
+            else if (lang == language.pl)
             {
+                MIfile.Header = "Plik";
+                MIexit.Header = "Zakończ";
+                MIrestore.Header = "Przywróć";
+                MIdefault_colors.Header = "Domyślne kolory kwadratu";
+                MIdefault_settings.Header = "Wszystkie ustawienia domyślne";
+                MIlanguage.Header = "Język";
+                MIenglish.Header = "Angielski";
+                MIpolish.Header = "Polski";
+                MIhelp.Header = "Pomoc";
+                MImanual.Header = "Instrukcja obsługi";
+                MIabout.Header = "O programie";
+                GBmain_settings.Header = "Ustawienia główne";
+                CHBLMB.Content = "Klik LPM";
+                CHBRMB.Content = "Klik PPM";
+                CHBdoubleLMB.Content = "Dwuklik LPM";
+                CHBholdLMB.Content = "Przytrzymanie LPM";
+                CHBholdRMB.Content = "Przytrzymanie PPM";
+                CHBscreen_panning.Content = "Przesuwanie ekranu";
+                Lcursor_idle_before_squares_appear.Text = "Czas bezczynności kursora zanim pojawią" +
+                    " się kwadraty [ms]:";
+                Ltime_to_start_mouse.Text = "Czas na rozpoczęcie ruchu myszą po pojawieniu się "
+                    + "kwadratów [ms]:";
+                Lcursor_time_in_square.Text = "Czas kursora w kwadracie do zarejestrowania kliknięcia [ms]:";
+                GBother_settings.Header = "Pozostałe ustawienia";
+                CHBrun_at_startup.Content = "Uruchom przy starcie komputera";
+                CHBstart_minimized.Content = "Uruchom zminimalizowany";
+                CHBminimize_to_tray.Content = "Minimalizuj do zasobnika systemowego";
+                CHBcheck_for_updates.Content = "Automatycznie sprawdzaj aktualizacje";
+                GBsquare_settings.Header = "Ustawienia kwadratów";
+                Lsquare_size.Content = "Rozmiar [px]:";
+                Lsquare_border.Content = "Szerokość krawędzi [px]:";
+                Lsquare_color1.Content = "Kolor 1:";
+                Lsquare_color2.Content = "Kolor 2:";
+                Lmin_square_size.Content = "Minimalny rozmiar [%]:";
+                GBrecommended_square_size.Header = "Zalecany rozmiar kwadratu";
+                Lscreen_size.Content = "Przekątna ekranu [cale]:";
+                contentLscreen_resolution = "Rozdzielczość ekranu: ";
                 //int x = Screen.PrimaryScreen.Bounds.Width;
                 //int y = Screen.PrimaryScreen.Bounds.Height;
-                //TBLscreen_resolution.Text = x + "x" + y;
+                Lscreen_resolution.Content = contentLscreen_resolution + x + "x" + y;
+                Bset_recommended_square.Content = "Ustaw zalecaną wielkość kwadratu";
+
+                error_title = "Błąd";
+                about_title = "O programie";
                 about_content = "Wersja: " + prog_version + "\nAutor: Mikołaj Magowski";
+                argb_error = "Wartość ARGB koloru musi być mniejsza od 0";
+                screen_size_error1 = "Przekątna ekranu musi być większa od 0";
+                screen_size_error2 = "Przekątna ekranu nie może być pusta";
+                cursor_idle_time_error = "Czas bezczynności kursora zanim pojawią się kwadraty nie może" +
+                    " być niższy niż ";
+                time_to_start_mouse_movement_error = "Czas na rozpoczęcie ruchu myszą nie może być "
+                    + "niższy niż ";
+                cursor_time_in_square_error = "Czas kursora w kwadracie do zarejestrowania kliknięcia"
+                    + " nie może byc niższy niż ";
+                square_size_error = "Rozmiar kwadratu nie może być niższy niż ";
+                square_border_error = "Rozmiar krawędzi kwadratu nie może być niższy niż ";
+                min_square_size_too_low_error = "Minimalny rozmiar kwadratu nie może być niższy niż ";
+                min_square_size_too_high_error = "Minimalny rozmiar kwadratu nie może być wyższy niż ";
+                loading_error_msg = " Błąd odczytu ustawień - wartości, które nie zostały wczytane " +
+                    "zostaną przywrócone do domyślnych i zapisane.";
             }
         }
 
         void set_instructions(language lang)
         {
-            if(lang == language.en)
+            if (lang == language.en)
             {
                 wm.Title = "User Guide";
                 wm.RTBinstructions.Source = new Uri("avares://ClicklessMouse/Assets/1en.md");
             }
             else if (lang == language.pl)
-                {
+            {
                 wm.Title = "Instrukcja obsługi";
                 wm.RTBinstructions.Source = new Uri("avares://ClicklessMouse/Assets/1pl.md");
             }
-            //else if(lang == language.pl)
-            //{
-            //    wm.Title = "Instrukcja obsługi";
-
-            //    FlowDocument fd = new FlowDocument();
-            //    Paragraph p1 = new Paragraph();
-            //    //Paragraph p2 = new Paragraph();
-            //    p1.Inlines.Add(new Run("Clickless Mouse ułatwia użytkowanie komputera osobą z RSI " +
-            //        "(repetitive strain injury),"
-            //        + " zespołem cieśni nadgarstka, niektórymi niepełnosprawnościami ruchowymi oraz"
-            //        + " innymi problemami zdrowotnymi."
-            //        + "\n\nTa aplikacja umożliwia używanie myszki bez klikania - jedynie poprzez"
-            //        + " jej przesuwanie."
-            //        + "\n\nPoprzez reagowanie na poruszanie myszą przez użytkownika program symuluje"
-            //        + " lewy/prawy przycisk myszy, podwójne kliknięcie lewym przyciskiem myszy "
-            //        + "oraz przytrzymywanie lewego/prawego przycisku myszy."
-            //        + "\n\nClickless Mouse można używać z klawiaturą wirtualną do pisania poprzez " +
-            //        "poruszanie myszką (np. Free Virtual Keyboard: "));
-
-            //    Hyperlink link = new Hyperlink();
-            //    link.IsEnabled = true;
-            //    link.Inlines.Add("https://freevirtualkeyboard.com/");
-            //    link.NavigateUri = new Uri("https://freevirtualkeyboard.com/");
-            //    link.RequestNavigate += (sender, args) => System.Diagnostics.Process.Start(args.Uri.ToString());
-            //    p1.Inlines.Add(link);
-
-            //    p1.Inlines.Add(new Run(").\n\nUżywając Clickless Mouse wraz z wirtualną klawiaturą"
-            //        + " użytkownik może w pełni kontrolować komputer poprzez poruszanie myszką."
-            //        + "\n\nKiedy chcesz kliknąć/przytrzymać przycisk myszy:"
-            //        + " zatrzymaj myszkę, poczekaj aż pojawią się kwadraty, a następnie przesuń"
-            //        + " kursor do wybranego kwadratu:"));
-
-            //    List a = new List();
-            //    a.MarkerStyle = TextMarkerStyle.Disc;
-            //    a.Padding = new Thickness(25, 0, 0, 0);
-            //    a.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Górny środkowy kwadrat = podwójne kliknięcie lewym przyciskiem myszy"))));
-            //    a.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Górny lewy kwadrat = kliknięcie lewym przyciskiem myszy"))));
-            //    a.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Górny prawy kwadrat = kliknięcie prawym przyciskiem myszy"))));
-            //    a.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Lewy kwadrat = włącz/wyłącz przytrzymanie lewego przycisku myszy"))));
-            //    a.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Prawy kwadrat = włącz/wyłącz przytrzymanie prawego przycisku myszy"))));
-
-            //    Bitmap bitmap = new Bitmap(AssetLoader.Open(new Uri("avares://ClicklessMouse/Assets/1pl.jpg")));
-            //    Image image = new Image();
-            //    image.Source = bitmap;
-            //    image.Width = 521;
-            //    //p2.Inlines.Add(image);
-
-            //    //p2.Inlines.Add(new Run("\nKiedy kursor myszy znajduje się wystarczająco długo"
-            //    //    + " w kwadracie (w zależności od ustawienia czasu kursora w kwadracie do rejestracji"
-            //    //    + " kliknięcia)"
-            //    //    + ", przesunie się automatycznie do poprzedniej pozycji, aby zasymulować"
-            //    //    + " akcję na podstawie ostatnio odwiedzonego kwadratu."
-            //    //    + "\n\nJeśli kursor myszy jest zbyt blisko górnej krawędzi ekranu, kwadraty"
-            //    //    + " dla kliknięcia LPM, kliknięcia PPM i dwukliknięcia LPM są pokazywane"
-            //    //    + " poniżej kursora myszy."
-            //    //    + "\n\nGdy kursor myszy jest zbyt blisko lewej lub prawej krawędzi ekranu"
-            //    //    + " rozmiar wszystkich kwadratów zostaje zmniejszony tak aby przynajmniej 25%"
-            //    //    + " kwadratu było widoczne. Maksymalne domyślne zmniejszenie rozmiaru kwadratu to"
-            //    //    + " 60% oryginalnej wielkości."
-            //    //    + "\nWielkość kwadratów nie zmniejsza się, jeśli kursor jest na tyle blisko"
-            //    //    + " krawędzi ekranu, że najmniejszy rozmiar nie wystarczy do pokazania kwadratu."
-            //    //    + "\n\nIstnieje możliwość wyłączenia niepotrzebnych kwadratów."
-            //    //    + "\n\nPrzesuwanie ekranu - gdy tryb ten jest włączony, przesunięcie kursora do" +
-            //    //    " krawędzi ekranu powoduje naciśnięcie klawiszy: góra dla górnej krawędzi, dół" +
-            //    //    " dla dolnej krawędzi, lewo dla lewej krawędzi i prawo dla prawej krawędzi." +
-            //    //    " Gdy przesuwanie ekranu jest aktywne, kwadraty nie są pokazywane kiedy kursor" +
-            //    //    " myszy znajduje się na krawędzi ekranu."
-            //    //    + "\n\nClickless Mouse działa tylko w programach i grach, które zostały"
-            //    //    + " uruchomione w trybie bezramkowym lub okienkowym (tryb pełnoekranowy"
-            //    //    + " nie jest wspierany)."));
-
-            //    Paragraph p3 = new Paragraph();
-            //    Bold b = new Bold();
-            //    b.Inlines.Add(new Run("Pierwsze kroki:"));
-            //    p3.Inlines.Add(b);
-
-            //    List a2 = new List();
-            //    a2.MarkerStyle = TextMarkerStyle.Decimal;
-            //    a2.Padding = new Thickness(25, 0, 0, 0);
-            //    a2.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Podaj przekątną ekranu, a następnie naciśnij przycisk 'Ustaw zalecaną wielkość " +
-            //        "kwadratu'."))));
-            //    a2.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Zdecyduj z jakich funkcji myszki chcesz skorzystać. Większość użytkowników wybiera"
-            //        + " 'Klik LPM', 'Klik PPM', 'Dwuklik LPM' oraz 'Przytrzymywanie LPM'."))));
-            //    a2.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Jeśli posiadasz niepełnosprawność ruchową rozważ zwiększenie czasu bezczynności" +
-            //        " kursora zanim pojawią się kwadraty, czasu na rozpoczęcie " +
-            //        "ruchu myszą po pojawieniu się kwadratów oraz rozmiaru kwadratów."))));
-
-            //    Paragraph p4 = new Paragraph();
-            //    b = new Bold();
-            //    b.Inlines.Add(new Run("Najniższe możliwe wartości (program ignoruje niższe wartości"
-            //        + " i używa poniższe zamiast nich):"));
-            //    p4.Inlines.Add(b);
-
-            //    List a3 = new List();
-            //    a3.MarkerStyle = TextMarkerStyle.Disc;
-            //    a3.Padding = new Thickness(25, 0, 0, 0);
-            //    a3.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Czas bezczynności kursora zanim pojawią się kwadraty [ms]: "
-            //        + lowest_cursor_idle_time_ms))));
-            //    a3.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Czas na rozpoczęcie ruchu myszą po pojawieniu się kwadratów [ms]: "
-            //        + lowest_time_to_start_mouse_movement_ms))));
-            //    a3.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Czas kursora w kwadracie do zarejestrowania kliknięcia [ms]: "
-            //        + lowest_cursor_time_in_square_ms))));
-            //    a3.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Rozmiar [px]: " + lowest_size))));
-            //    a3.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Szerokość krawędzi [px]: " + lowest_border_width))));
-            //    a3.ListItems.Add(new ListItem(new Paragraph(new Run(
-            //        "Minimalny rozmiar [%]: " + lowest_min_square_size_percents))));
-
-            //    fd.Blocks.Add(p1);
-            //    fd.Blocks.Add(a);
-            //    //fd.Blocks.Add(p2);
-            //    fd.Blocks.Add(p3);
-            //    fd.Blocks.Add(a2);
-            //    fd.Blocks.Add(p4);
-            //    fd.Blocks.Add(a3);
-            //    //wm.RTBinstructions.Document = fd;
-            //}
         }
     }
 }
