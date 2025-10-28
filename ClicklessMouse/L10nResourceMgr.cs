2a6c22f8308f78d7863b8a0638e28ef7f84acbc3
Garrett Anderson <gband85@mailfence.com>
2025-10-27 20:47:21 -0500

    add L10nResourceMgr.cs

---
+++ ClicklessMouse/L10nResourceMgr.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ClicklessMouse.Properties;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace ClicklessMouse {
+    public class L10nResourceMgr : INotifyPropertyChanged {
+        private L10nResourceMgr() {
+            Culture = CultureInfo.CurrentCulture;
+        }
+
+        public CultureInfo Culture
+        {
+            get => CultureInfo.CurrentUICulture;
+            set
+            {
+                CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+            }
+        }
+
+        public static L10nResourceMgr Instance { get; } = new();
+
+        public object this[string resourceKey]
+            => Resources.ResourceManager.GetObject(resourceKey, Culture) ?? Array.Empty<byte>();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
