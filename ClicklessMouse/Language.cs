2f8f653ce87a126c51f81f41d253b2e3c986682f
Garrett Anderson <gband85@mailfence.com>
2025-10-29 15:50:57 -0500

    fix manual contents

--- ClicklessMouse/Language.cs
+++ ClicklessMouse/Language.cs
@@ -1,5 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
 using System.Globalization;
+using System.IO;
+using Avalonia.Platform;
 
 namespace ClicklessMouse
 {
@@ -13,10 +16,28 @@ namespace ClicklessMouse
 
         void change_language(UILanguage lang)
         {
+            Uri asset_uri;
+            Stream stream;
+            StreamReader reader;
             if (lang == UILanguage.en)
+            {
                 L10nResourceMgr.Instance.Culture = new CultureInfo("en-US");
+                asset_uri=new Uri("avares://ClicklessMouse/Assets/1en.md");
+                stream =AssetLoader.Open(asset_uri);
+                reader = new StreamReader(stream);
+                Wmanual.RTBinstructions.Markdown=reader.ReadToEnd();
+            }
+
+
             else if (lang == UILanguage.pl)
+            {
                 L10nResourceMgr.Instance.Culture = new CultureInfo("pl-PL");
+                asset_uri = new Uri("avares://ClicklessMouse/Assets/1pl.md");
+                stream = AssetLoader.Open(asset_uri);
+                reader = new StreamReader(stream);
+Wmanual.RTBinstructions.Markdown = reader.ReadToEnd();
+            }
+            
         }
     }
 }
