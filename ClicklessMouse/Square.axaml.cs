a12a602df2113f453b22607ec1be85afb716f709
Garrett Anderson <gband85@mailfence.com>
2025-11-23 20:48:57 -0600

    fix off-by-one error

--- ClicklessMouse/Square.axaml.cs
+++ ClicklessMouse/Square.axaml.cs
@@ -57,8 +57,8 @@ Console.Write("");
 //             IntPtr display;
 // X11.Window w;
 //             Atom net_client_list;
-             long long_offset = 0;
-             long long_length = long.MaxValue;
+             long long_offset = 0L;
+             long long_length = 256L;
              bool delete=false;
 //             Atom req_type; 
 //             
@@ -66,7 +66,7 @@ Console.Write("");
             IntPtr prop_return;
              Atom actual_type_return=new Atom();
              int actual_format_return=0;
-             ulong nitems_return = new();
+             ulong nitems_return = 0UL;
                  ulong bytes_after_return=0UL;
              List<Window> prop_return_list= [];
 
@@ -83,18 +83,19 @@ Atom net_client_list = Xlib.XInternAtom(display, "_NET_CLIENT_LIST", true);
            int result = InputX11.XGetWindowProperty(display, w, net_client_list, long_offset, long_length, delete, req_type2, ref actual_type_return, ref actual_format_return,
                ref nitems_return, ref bytes_after_return, out prop_return);
             //if (result && actual_type_return == XA_WINDOW) ;
-            for (int i = 0; i < (int)nitems_return; i++)
+            ulong count = nitems_return - 1UL;
+            for (ulong i = 0; i <= count; i++)
             {
-                IntPtr ptr = new IntPtr(prop_return.ToInt64() +  (i * 8));
+                IntPtr ptr = new IntPtr(prop_return.ToInt64() +  (int)i * 8);
                // ulong windowId = (ulong)Marshal.ReadInt64(ptr);
                 // Window g = (Window)windowId;
                 // Console.WriteLine($"Id:");
-                prop_return_list.Add((Window) Marshal.ReadInt64(prop_return,i*8));
+                prop_return_list.Add((Window)ptr);
                 InputX11.XGetWindowProperty(display, (Window)Marshal.ReadInt64(ptr), prop, long_offset, long_length,
                     false, req_type2, ref actual_type_return, ref actual_format_return, ref nitems_return,
                     ref bytes_after_return, out prop_return2);
                 // var t = Marshal.ReadInt64(prop_return2, 8).ToString();
-                Xlib.XFetchName(display, (Window)Marshal.ReadInt64(prop_return,i*8), ref window_name);
+                Xlib.XFetchName(display, (Window)Marshal.ReadInt64(prop_return2,(int)i*8), ref window_name);
 Console.WriteLine(window_name);
             }
             
