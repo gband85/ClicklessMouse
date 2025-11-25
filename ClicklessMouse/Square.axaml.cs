9be00fdb69851f3c6656ff95fb19b736226ad64d
Garrett Anderson <gband85@mailfence.com>
2025-11-25 17:10:05 -0600

    create new vars

--- ClicklessMouse/Square.axaml.cs
+++ ClicklessMouse/Square.axaml.cs
@@ -67,6 +67,7 @@ Console.Write("");
              Atom actual_type_return=new Atom();
              int actual_format_return=0;
              ulong nitems_return = 0UL;
+                 ulong nitems_return2 = 0UL;
                  ulong bytes_after_return=0UL;
              List<Window> prop_return_list= [];
 
@@ -92,7 +93,7 @@ Atom net_client_list = Xlib.XInternAtom(display, "_NET_CLIENT_LIST", true);
                 // Console.WriteLine($"Id:");
                 prop_return_list.Add((Window)Marshal.ReadInt64(ptr));
                 InputX11.XGetWindowProperty(display, (Window)Marshal.ReadInt64(ptr), prop, long_offset, long_length,
-                    false, req_type2, ref actual_type_return, ref actual_format_return, ref nitems_return,
+                    false, req_type2, ref actual_type_return, ref actual_format_return, ref nitems_return2,
                     ref bytes_after_return, out prop_return2);
                 // var t = Marshal.ReadInt64(prop_return2, 8).ToString();
                 Xlib.XFetchName(display, (Window)Marshal.ReadInt64(prop_return2,(int)i*8), ref window_name);
