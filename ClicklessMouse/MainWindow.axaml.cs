42a6fea2583f07c9c396c4ad8a0d2ce5e888a24e
Garrett Anderson <gband85@mailfence.com>
2026-05-12 19:19:07 -0500

    set data file locations per os

--- ClicklessMouse/MainWindow.axaml.cs
+++ ClicklessMouse/MainWindow.axaml.cs
@@ -126,6 +126,15 @@ namespace ClicklessMouse
             InitializeComponent();
             DataContext = this;
 
+#if _WINDOWS
+           app_folder_path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), prog_name); 
+            default_settings_path=Path.Combine(AppContext.BaseDirectory,default_settings_filename);
+
+#elif _LINUX
+            app_folder_path = Path.Combine(Environment.GetEnvironmentVariable("HOME"), ".config", prog_name.Replace(" ", String.Empty));
+            default_settings_path = Path.Combine("/usr/share", prog_name.Replace(" ", String.Empty), default_settings_filename);
+
+#endif
             //Stream iconStream = System.Windows.Application.GetResourceStream(
             //    new Uri("pack://application:,,,/ClicklessMouse;component/clickless_mouse.ico")).Stream;
             //ni.Icon = new System.Drawing.Icon(iconStream);
