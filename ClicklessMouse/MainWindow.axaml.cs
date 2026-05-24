e76108b955eeaec2693d7408fafa45a42662cab7
Garrett Anderson <gband85@mailfence.com>
2026-05-24 16:42:25 -0500

    fix resource path

--- ClicklessMouse/MainWindow.axaml.cs
+++ ClicklessMouse/MainWindow.axaml.cs
@@ -130,7 +130,7 @@ namespace ClicklessMouse
 
 #if _WINDOWS
            app_folder_path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), prog_name); 
-           default_settings_path=Path.Combine(AppContext.BaseDirectory,"Assets",default_settings_filename);
+           default_settings_path=Path.Combine(AppContext.BaseDirectory,default_settings_filename);
 
 #elif _LINUX
             app_folder_path = Path.Combine(Environment.GetEnvironmentVariable("HOME"), ".config", prog_name.Replace(" ", String.Empty));
