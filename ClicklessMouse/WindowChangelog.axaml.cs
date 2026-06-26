108b23c6a530abcae191d9cf2f38bf198315fbdc
Garrett Anderson <gband85@mailfence.com>
2026-06-26 16:59:17 -0500

    update changelog and eula

--- ClicklessMouse/WindowChangelog.axaml.cs
+++ ClicklessMouse/WindowChangelog.axaml.cs
@@ -14,25 +14,36 @@ public partial class WindowChangelog : Window
 
             TB.IsReadOnly = true;
 
-            TB.Text = "All notable changes to Clickless Mouse will be documented here."
-            + "\n\n[2.2] - Januray 28, 2024:"
-            + "\n- Added automatic check for updates."
-            + "\n- Changed recommended square size."
-            + "\n- Improved UI."
-            + "\n\n[2.1] - December 7, 2023:"
-            + "\n- Clickless Mouse from now on requires administrator rights to run."
-            + "\n- Default \"Cursor time in square to register a click\" is now 100ms."
-            + "\n- Improved mouse button holding and releasing."
-            + "\n- Other minor improvements.";
+            TB.Text = """
+                      All notable changes to Clickless Mouse will be documented here.
+                      
+                      [3.2] - June 26, 2026:
+                      - Ported to AvaloniaUI.
+                      - Now cross-platform for Windows and X11 on Linux.
+                      - Other minor improvements.
+                      
+                      [2.2] - Januray 28, 2024:
+                      - Added automatic check for updates.
+                      - Changed recommended square size.
+                      - Improved UI.
+                      
+                      [2.1] - December 7, 2023:
+                      - Clickless Mouse from now on requires administrator rights to run.
+                      - Default "Cursor time in square to register a click" is now 100ms.
+                      - Improved mouse button holding and releasing.
+                      - Other minor improvements.
+                      """;
         }
         catch (Exception ex)
         {
             ShowError(ex);
         }
     }
+
     async private void ShowError(Exception ex)
     {
-        var box = MessageBoxManager.GetMessageBoxStandard("Error WC001", ex.Message, MsBox.Avalonia.Enums.ButtonEnum.Ok, MsBox.Avalonia.Enums.Icon.Error);
+        var box = MessageBoxManager.GetMessageBoxStandard("Error WC001", ex.Message, MsBox.Avalonia.Enums.ButtonEnum.Ok,
+            MsBox.Avalonia.Enums.Icon.Error);
         await box.ShowAsync();
     }
 }
